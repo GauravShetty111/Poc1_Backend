bbdf64ca1a7f14f218c3b2dfa25b6d7661fb2e8e
GauravShetty111 <gaurav.shetty@gleecus.com>
2025-11-13 19:12:49 +0530

    (feat):The JWt Auth Done and its been converted Dynamic

--- main.py
+++ main.py
@@ -1,15 +1,56 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from db.connect import supabase
+from services.emailService import sendEmail
+from pydantic import BaseModel
+
+
+class UserModel(BaseModel):
+    user_id:int
+    username:str
+    password:str
+
+
 
 app =FastAPI()
 
 @app.get("/health-check")
 async def root():
     # addedData = supabase.table('users').insert({"user_id":20,"username": "gaurav","profile_id":20, "table_name": "nothing"}).execute()
+    sendEmail("gaurav.shetty@gleecus.com","Registration")
     response =  supabase.table('users').select("*").execute()
     
     return response.data
 
 
 
+
+@app.post("/register")
+async def registerRoute(user:UserModel):
+    return user
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@app.post("/login")
+async def loginRoute():
+    return {"message": "Login "}
+
+@app.post("/refresh")
+async def refreshRoute():
+    return {"message": "Refresh Token"}
